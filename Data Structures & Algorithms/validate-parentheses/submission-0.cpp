6c020a5c879a19903ac491489f0cd8a8abce061d
Mauro Malek <mauriciolegend1@gmail.com>
2026-06-20 13:11:00 -0700

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, int> mp = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        stack<char> st;
+
+        for (char c: s){
+            if (mp.count(c)){
+                if(!st.empty() && st.top() == mp[c]) st.pop();
+                else return false;
+            } else st.push(c);
+        }
+        return st.empty();
+    }
+};
